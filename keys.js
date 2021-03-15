953f48e6480b9760de9fa2a95a279ce4c5a2e8cf
aman <aman282571@gmail.com>
2021-03-15 13:08:36 +0530

    inital commit

---
+++ keys.js
@@ -0,0 +1,7 @@
+mongo_uri =
+	'mongodb+srv://aman28:Aman28@cluster0.ohyhb.mongodb.net/users?retryWrites=true&w=majority';
+client_id =
+	'295559939515-vl52afq5jgl3au7p28m0t4s0gnendn0c.apps.googleusercontent.com';
+client_secret = 'UdJKO6E8g_evT2W2FOWtHmTX';
+callback_url = 'http://localhost:3000/auth/google/redirect';
+module.exports = { client_id, mongo_uri, client_secret, callback_url };
